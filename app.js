57d9051bbfa4c2a717e02fa50ca1c681a5290959
Taiabevacqua <natalia.bevacqua@gmail.com>
2023-11-15 15:55:32 -0300

    se cambió el nombre de la ruta home por index en app.js

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const PORT =3030;
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')))
 app.get('/footer',(req,res) => res.sendFile(path.join(__dirname,'views','footer.html')))
 app.get('/header',(req,res) => res.sendFile(path.join(__dirname,'views','header.html')))
-app.get('/index',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')))
 app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
 app.get('/productCart',(req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')))
 app.get('/productDetail',(req,res) => res.sendFile(path.join(__dirname,'views','productDetail.html')))
