631c23bbf669d114aa96e1976cc24af123ecc2cf
Jairoabarca <jairo.caravaca@hotmail.com>
2023-12-04 09:22:09 -0300

    agregando modificaciones

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT =3030;
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views' , 'index.html')))
+app.get('/index',(req,res) => res.sendFile(path.join(__dirname, 'views' , 'index.html')))
 app.get('/footer',(req,res) => res.sendFile(path.join(__dirname, 'views' , 'partials' , 'footer.html')))
 app.get('/header',(req,res) => res.sendFile(path.join(__dirname, 'views' , 'partials' , 'header.html')))
 app.get('/login',(req,res) => res.sendFile(path.join(__dirname, 'views' , 'login.html')))
