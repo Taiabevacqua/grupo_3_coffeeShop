e5d040acc8efc11c85f5649a2991e622dccdb486
Taiabevacqua <natalia.bevacqua@gmail.com>
2024-02-07 18:38:29 -0300

    corrigiendo slide

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -6,88 +6,86 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-const writeJson=(database) => {
-	fs.writeFileSync(path.join(__dirname,'../data/products.json'), JSON.stringify(database),"utf-8")
+const writeJson = (database) => {
+    fs.writeFileSync(path.join(__dirname, '../data/products.json'), JSON.stringify(database), "utf-8")
 }
 
 const Product = require("../data/Product");
 
-function pushProducts (parametro){
-	fs.writeFileSync("./src/data/products.json", JSON.stringify(parametro, null, 3), "utf-8");}
+function pushProducts(parametro) {
+    fs.writeFileSync("./src/data/products.json", JSON.stringify(parametro, null, 3), "utf-8");
+}
 
-module.exports={
-    add : (req,res) => {
+module.exports = {
+    add: (req, res) => {
         return res.render('products/product-add')
         let lastId = 1;
-		products.forEach(product => {
-			if(product.id > lastId){
-				lastId=product.id
-			}
-		})
-		
-		let{name, price, descuento, category, description,} =req.body
+        products.forEach(product => {
+            if (product.id > lastId) {
+                lastId = product.id
+            }
+        })
 
-		let newProduct = {
-			id: lastId+1,
-			name,
-			price,
-			descuento,
-			category,
-			description,
-			imagen: req.file ? req.file.filename : "default-image.png"
-		}
+        let { name, price, descuento, category, description, } = req.body
 
-		products.push(newProduct)
+        let newProduct = {
+            id: lastId + 1,
+            name,
+            price,
+            descuento,
+            category,
+            description,
+            imagen: req.file ? req.file.filename : "default-image.png"
+        }
 
-		writeJson(products)
+        products.push(newProduct)
 
-		res.redirect('/dashboard')
-     },
-    detail: (req,res) =>{
+        writeJson(products)
+
+        res.redirect('/dashboard')
+    },
+    detail: (req, res) => {
         return res.render('products/productDetail')
     },
-    cafeteras : (req, res) => {
-        return res.render('products/cafeteras',{
-            productos, 
+    cafeteras: (req, res) => {
+        return res.render('products/cafeteras', {
+            productos,
         })
     },
-    capsulas : (req, res) => {
-        return res.render('products/capsulas',{
-            productos , 
+    capsulas: (req, res) => {
+        return res.render('products/capsulas', {
+            productos,
         })
     },
-    cafeengrano : (req, res) => {
-        return res.render('products/cafeengrano',{
-            products , 
+    cafeengrano: (req, res) => {
+        return res.render('products/cafeengrano', {
+            products,
         })
     },
-    
-    
+
+    edit: (req, res) => {
 
 
-    edit : (req, res) => {
-        
-        
         const product = products.find(product => product.id === +req.params.id)
-         res.render('products/products-edit', {
-           product
-            
-        }) 
+        res.render('products/products-edit', {
+            product
+
+        })
     },
 
-    update: (req,res) => {
-     
-    return console.log(req.body);
+    update: (req, res) => {
+
+        return console.log(req.body);
     },
 
 
 
-    create : (req,res) => {
-    
-        const{name, description, price, descuento, category, imagen, stock, sabor}= req.body
-        
-        const newProduct= new Product( name, description, price, descuento, category, imagen, stock, sabor)
-        const products=leerJSON('products')
+    create: (req, res) => {
+
+        const { name, description, price, descuento, category, imagen, stock, sabor } = req.body
+
+        const newProduct = new Product(name, description, price, descuento, category, imagen, stock, sabor)
+        const products = leerJSON('products')
 
         products.push(newProduct)
 
@@ -96,28 +94,29 @@ module.exports={
         return res.redirect('/dashboard')
 
     },
-    store : (req, res) => {
+    store: (req, res) => {
+        const creador = require('../data/creador');
+        const imagen = req.file;
+        const lastID = products[products.length -1].id;
 
-    const creador = require('../data/creador');
-	const imagen = req.file;
-	const {nombre, categoria, precio, stock, sabores,descuento, descripcion} = req.body;
-	const nuevoCreador = new creador(nombre, imagen,categoria, precio, stock, sabores,descuento, descripcion);
-	productos.push(nuevoCreador);
-	pushProducts(productos);
-	return res.redirect("/")
-    
-    },
-    todos : (req,res) => {
-        const products=leerJSON('products')
+        const { nombre, categoria, precio, stock, sabores, descuento, descripcion } = req.body;
+        const nuevoCreador = new creador(nombre, imagen, categoria, precio, stock, sabores, descuento, descripcion);
+        productos.push(nuevoCreador);
+        pushProducts(productos);
+        return res.redirect("/")
 
-        res.render('products/todos', {products})
     },
-    search: (req, res) => {        
-		const {keywords} = req.query;
-        
-		return res.render('', {
-			productos : productos.filter(producto => producto.nombre.toLowerCase().includes(keywords.toLowerCase()) ||  producto.descripcion.toLowerCase().includes(keywords.toLowerCase())), 
-			keywords            
-		})
-	}    
+    todos: (req, res) => {
+        const products = leerJSON('products')
+
+        res.render('products/todos', { products })
+    },
+    search: (req, res) => {
+        const { keywords } = req.query;
+
+        return res.render('', {
+            productos: productos.filter(producto => producto.nombre.toLowerCase().includes(keywords.toLowerCase()) || producto.descripcion.toLowerCase().includes(keywords.toLowerCase())),
+            keywords
+        })
+    }
 }
