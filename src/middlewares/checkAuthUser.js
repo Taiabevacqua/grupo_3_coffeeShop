016d6a29ad25b048a50642a88feb28106ac11b1d
Naty <144572774+Taiabevacqua@users.noreply.github.com>
2024-03-23 21:00:12 -0300

    Revert "Develop"

--- src/middlewares/checkAuthUser.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req,res, next) => {
-    if(req.session.userLogin){
-        return res.redirect('/usuarios/perfil')
-    }
-    return next()
-
-}
--- src/middlewares/cookiesCheck.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req,res,next) => {
-    if(req.cookies.GranodeOro_user){
-        req.session.userLogin = req.cookies.GranodeOro_user
-    }
-
-    next()
-}
